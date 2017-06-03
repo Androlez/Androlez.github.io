ee9c95bb85d8b15eb5297b11050051a8dd07ad0d
Androlez <andres.gonzalez.valenzuela@gmail.com>
2017-06-02 21:05:35 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+//Ejercicio Base 1
+//Taller Intro a la programación en p5
+//Puerto Varas C
+
+//var x=0;
+
+
+function setup() {
+createCanvas(800,500);
+
+
+}
+
+function draw() {
+	background(255,178,145);
+	fill(100,200,255);
+ellipse(mouseX,mouseY,200,200); 
+//x=x+1; 
+}
